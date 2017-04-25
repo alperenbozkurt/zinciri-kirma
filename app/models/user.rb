7259f2d27a158557e83f155cd6e55a08ec1dcbb0
alperenbozkurt <bozkurttalperen@gmail.com>
2017-04-26 02:06:26 +0300

    Hadi son olsun Artık yarın işim var.

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,6 @@ class User < ApplicationRecord
   end
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default_image.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/default_image.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
